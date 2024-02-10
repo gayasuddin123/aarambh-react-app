04d10e386bb30a7b7daa24ebab83e59aab6114ab
fastX <akashkush6272@gmail.com>
2024-02-11 03:55:05 +0530

    activeslider

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import HeroSection from './component/Herosection'
+import Testimonials from './component/testimonials'
+import Footer from './component/Footer';
+import ActiveSlider from './component/ActiveSlider';
+
+function App() {
+  return (
+    <>
+    <HeroSection/>
+    <Testimonials/>
+    <ActiveSlider/>
+    
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/constants/activeslider.js
@@ -0,0 +1,59 @@
+import {
+  RxCrop,
+  RxDesktop,
+  RxPencil2,
+  RxReader,
+  RxRocket,
+  RxAccessibility,
+} from "react-icons/rx";
+
+import arrpic1 from "../assets/arrpic1.jpg";
+import arrpic2 from "../assets/arrpic2.jpg";
+import arrpic3 from "../assets/arrpic3.jpg";
+import arrpic4 from "../assets/arrpic4.jpg";
+import arrpic5 from "../assets/arrpic5.jpg";
+import arrpic6 from "../assets/arrpic6.jpg";
+
+ 
+
+export const ServiceData = [
+  {
+    icon: RxCrop,
+    title: "Development",
+    content: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
+    backgroundImage: arrpic1,
+  },
+  {
+    icon: RxPencil2,
+    title: "Branding",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: arrpic2,
+  },
+  {
+    icon: RxDesktop,
+    title: "Design",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: arrpic3,
+  },
+  {
+    icon: RxReader,
+    title: "Seo",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: arrpic4,
+  },
+  {
+    icon: RxAccessibility,
+    title: "Management",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: arrpic5,
+  },
+  {
+    icon: RxRocket,
+    title: "Production",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    backgroundImage: arrpic6,
+  },
+];
+
+
+// {<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>}
