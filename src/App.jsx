3ff4cb9045cfabf6051492f5966269896adf4f67
fastX <akashkush6272@gmail.com>
2024-02-11 15:10:05 +0530

    activeslide

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import HeroSection from './component/Herosection'
 import Testimonials from './component/testimonials'
 import Footer from './component/Footer';
-import ActiveSlider from './component/ActiveSlider';
+ import Mainactiveslider from './component/activeslider/mainactiveslider';
 
 function App() {
   return (
     <>
     <HeroSection/>
     <Testimonials/>
-    <ActiveSlider/>
-    
+    <Mainactiveslider/>
+    <Mainactiveslider/>
     <Footer/>
     </>
   )
 }
 
-export default App
+export default App;
