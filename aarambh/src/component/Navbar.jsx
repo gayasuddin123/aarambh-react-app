a044b98e7ae5ded3964135716ba4839f03dfa305
Gayasuddin <gyasuansari145@gmail.com>
2024-02-11 11:40:05 +0530

    aa

--- aarambh/src/component/Navbar.jsx
+++ aarambh/src/component/Navbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Button from './Button'
 function Navbar() {
   const hamburger = () => {
 
@@ -41,11 +40,11 @@ function Navbar() {
         <div className="logo">
           <img src="../public/arambh-white.png" className='h-10' alt="" />
         </div>
-        <div className="flex w-60 text-white gap-14 items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+        <div className="flex w-24 text-white gap-14 items-center lg:w-60 ">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 ">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
           </svg>
-          <Button />
+          <button className='w-20 h-9 bg-slate-600 hidden lg:block rounded-md hover:bg-slate-300 hover:text-black'>Sign In</button>
         </div>
       </div>
     </>
