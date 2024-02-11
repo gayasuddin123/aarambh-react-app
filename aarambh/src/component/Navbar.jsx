d4ad1c848083fdedcf507cec3db0be71877c6e86
Gayasuddin <gyasuansari145@gmail.com>
2024-02-11 11:29:55 +0530

    k

--- aarambh/src/component/Navbar.jsx
+++ aarambh/src/component/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
   }
   return (
     <>
-      <div className='w-screen h-11 bg-slate-800 flex items-center justify-between group'>
+      <div className='w-screen h-14 bg-slate-800 flex items-center justify-between group'>
         <div className=" text-white block  lg:hidden"  onClick={hamburger()}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-8 ">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
