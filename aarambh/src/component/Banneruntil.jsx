836729bbe7b640748e4962d8028af5f1d0ef89f8
Anshuman-Ai <chillbeast29@gmail.com>
2024-02-11 22:09:00 +0530

    banner change

--- aarambh/src/component/Banneruntil.jsx
+++ aarambh/src/component/Banneruntil.jsx
@@ -39,22 +39,29 @@ const CountdownBanner = () => {
   }
 
   return (
-    <div className="bg-slate-900 text-white py-4 text-center">
-      <div className="container mx-auto">
-        <p className="text-lg md:text-xl">Registration Starts In:</p>
-        <div className="flex justify-center mt-2">
-          <div className="bg-white text-blue-500 font-bold text-2xl md:text-4xl rounded-full px-4 py-2 mr-2">
+    <div className="container mx-auto bg-slate-900 mr-2">
+      <div className="flex justify-between items-center mt-2">
+        <div className='md:text-4xl text-white py-2 px-4 text-left'>
+          <h1 className="text-lg md:text-xl">Registration Starts In:</h1>
+        </div>
+        <div className="flex">
+          <div className="bg-slate-900 text-blue-500 font-bold text-xl md:text-2xl rounded-full px-4 py-2 mr-2">
             {timeRemaining.days} <span className="text-xs md:text-lg">Days</span>
           </div>
-          <div className="bg-white text-blue-500 font-bold text-2xl md:text-4xl rounded-full px-4 py-2 mr-2">
+          <div className="bg-slate-900 text-blue-500 font-bold text-xl md:text-2xl rounded-full px-4 py-2 mr-2">
             {timeRemaining.hours} <span className="text-xs md:text-lg">Hours</span>
           </div>
-          <div className="bg-white text-blue-500 font-bold text-2xl md:text-4xl rounded-full px-4 py-2 mr-2">
+          <div className="bg-slate-900 text-blue-500 font-bold text-xl md:text-2xl rounded-full px-4 py-2 mr-2">
             {timeRemaining.minutes} <span className="text-xs md:text-lg">Minutes</span>
           </div>
-          <div className="bg-white text-blue-500 font-bold text-2xl md:text-4xl rounded-full px-4 py-2">
+          <div className="bg-slate-900 text-blue-500 font-bold text-2xl md:text-2xl rounded-full px-4 py-2">
             {timeRemaining.seconds} <span className="text-xs md:text-lg">Seconds</span>
           </div>
+        </div>
+        <div className="flex items-center">
+          <a href="#" className="bg-slate-300 hover:bg-slate-700 text-gray-900 hover:text-gray-800 font-bold rounded-full px-5 py-1 mr-2">
+          <p>Register</p>
+          </a>
         </div>
       </div>
     </div>
--- aarambh/src/component/Roadmap.jsx
+++ aarambh/src/component/Roadmap.jsx
@@ -4,7 +4,7 @@ function Banner() {
   return (
     <div className="bg-gray-100 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <h2 className="text-3xl md:text-4xl font-bold text-center mb-8"> RoadMap for Developing into Professional</h2>
-      <img src="Month 1.png" alt="Banner" className="rounded-lg shadow-lg" />
+      <img src="Month 6.png" alt="Banner" className="rounded-lg shadow-lg" />
     </div>
   );
 }
